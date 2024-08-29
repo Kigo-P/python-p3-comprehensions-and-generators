a0f7e9a86e86198bc3bb5041590a9b3dff46ebed
Kigo-P <peterkigo33@gmail.com>
2024-08-29 16:39:22 +0300

    uses list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # for elements in num_list:
+    #     if elements%2 == 0:
+    #         print(elements)
+    return[elements for elements in num_list if elements%2 == 0]
+
+print(return_evens([1,2,3,4,5,6,7,8,9,10]))  
 
 def make_exclamation(sentence_list):
-    pass
+    # for word in sentence_list:
+    #     if len(sentence_list) > 0:
+    #         print(f"{word}!")
+    return [f"{word}!" for word in sentence_list if len(sentence_list) > 0]
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
